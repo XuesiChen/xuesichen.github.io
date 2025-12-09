30e2a3237c4b1a5a303e7e21e58b35bac80131d4
XuesiChen <45868889+XuesiChen@users.noreply.github.com>
2025-12-09 18:34:45 +0000

    Deploying to gh-pages from @ XuesiChen/xuesichen.github.io@fe908890dc1c1cf80b6722ac0ff88053027b8f06 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "XuesiChen/https-xuesichen.github.io-",
+    "data-repo": "XuesiChen/xuesichen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
